b1c8bb10b8dc610931c12eedbbb13606adb213cd
CarlosAraki <carlosarak@gmail.com>
2017-05-17 13:39:45 -0300

    correção de bugs

--- lab9/main.vhd
+++ lab9/main.vhd
@@ -1,3 +1,6 @@
+--Nome:Carlos Vinícius Araki Oliveira RA:160141
+--Nome:Cleber França Carvalho RA:145739
+
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 USE ieee.std_logic_signed.all ;
@@ -75,7 +78,7 @@ SIGNAL EEND: STD_LOGIC_VECTOR(3 DOWNTO 0);
 	SELM1 <=SW(14);
 	SELRB <=SW(15);
 	SELRA <= SW(16);
-	ADDSUBR <=SW(17);
+	ADDSUBR <=SW(17);   ---obs: ligado é soma desligado é sub
 	CLKZ <=NOT KEY(0);
 	CLKM <=NOT KEY(1);
 	CLKR <= NOT KEY(2);
