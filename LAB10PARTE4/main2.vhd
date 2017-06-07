4fc7343c40a8d4d779869eb3e3bf5d76f3ca549b
Carlos Araki <carlosarak@gmail.com>
2017-06-07 14:50:36 -0300

    final

--- LAB10PARTE4/main2.vhd
+++ LAB10PARTE4/main2.vhd
@@ -10,6 +10,8 @@ USE ieee.std_logic_signed.all;
 ENTITY parte4 IS
 	PORT (
 				Clock,Resetn,Run:in STD_LOGIC;
+				SW: IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+				CLOCK_50: IN STD_LOGIC;
 				LEDR : out std_logic_vector(15 downto 0);
 				HEX0 :out std_LOGIC_VECTOR(6 downto 0);
 				HEX1 :out std_LOGIC_VECTOR(6 downto 0);
@@ -28,8 +30,9 @@ ARCHITECTURE Behavior OF parte4 IS
 
 component seg7_scroll IS
 	PORT (
-			sel1,sel2,sel3,sel4,sel5,sel6,sel7,sel8:in std_LOGIC;
-			Clk,Clear : IN STD_LOGIC;
+			sel1:in std_LOGIC;
+			Clk,Clear,En : IN STD_LOGIC;
+			qualhex:in Std_logic_vector(2 downto 0);
 			entrada1:in std_LOGIC_VECTOR(6 downto 0);
 			Display0:OUT STD_LOGIC_VECTOR(6 DOWNTO 0);
 			Display1:OUT STD_LOGIC_VECTOR(6 DOWNTO 0);
@@ -69,30 +72,34 @@ COMPONENT reg IS											--COMPONENT
 			q : OUT std_logic_vector(15 downto 0));
 	END COMPONENT;
 
+COMPONENT port_n IS
+		PORT ( chaves : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+				memmoria: in std_logic_vector(15 downto 0);
+				sel,Clk,Clear: in STD_LOGIC;
+				saida: out std_logic_vector(15 downto 0)
+			   );
+	END COMPONENT;
 
-signal auxend,DINN,AddroutT,DoutT: std_logic_vector(15 downto 0);
-signal notstall,addstall,W_DD,Wren,por,sor,E,h0or,h1or,h2or,h3or,h4or,h5or,h6or,h7or: STD_LOGIC;
+
+
+signal auxend,DINN,AddroutT,DoutT,saidaport: std_logic_vector(15 downto 0);
+signal notstall,addstall,W_DD,Wren,por,sor,E,h0or,seleci,CLOCK: STD_LOGIC;
 
 
 BEGIN
-	proc1: PROC port map(DINN,AddroutT,DoutT,Resetn,Clock,Run,addstall,W_DD,Done);
+	Clock<=CLOCK_50;
+	proc1: PROC port map(saidaport,AddroutT,DoutT,Resetn,Clock,Run,addstall,W_DD,Done);
 	por<= not(auxend(15) or auxend(14) or auxend(13) or auxend(12));
 	sor<= not(auxend(15) or auxend(14) or auxend(13) or not(auxend(12)));
 	h0or<=not(auxend(15) or auxend(14) or not(auxend(13)) or auxend(12)); --0010
-	h1or<=not(auxend(15) or auxend(14) or not(auxend(13)) or not(auxend(12))); --0011
-	h2or<=not(auxend(15) or not(auxend(14)) or auxend(13) or auxend(12));    --0100
-	h3or<=not(auxend(15) or not(auxend(14)) or auxend(13) or not(auxend(12))); --0101
-	h4or<=not(auxend(15) or not(auxend(14)) or not(auxend(13)) or auxend(12)); --0110
-	h5or<=not(auxend(15) or not(auxend(14)) or not(auxend(13)) or not(auxend(12))); --0111
-	h6or<=not(not(auxend(15)) or auxend(14) or auxend(13) or auxend(12)); --1000
-	h7or<=not(not(auxend(15)) or auxend(14) or auxend(13) or not(auxend(12))); --1001
-
+	seleci<= not(auxend(15) or auxend(14) or NOT(auxend(13)) or NOT(auxend(12)));--0011
 	Wren<= por and W_DD;
 	E<= W_DD and sor;
 	notstall <= not(addstall);
 	regaux: reg port map(notstall,Clock,Resetn,AddroutT,auxend);
 	--meus IOs
+	portn: port_n port map(SW,DINN,seleci,Clock,Resetn,saidaport);
 	led: reg port map(E,Clock,Resetn,DoutT,LEDR);
 	memoria: memRAM port map(AddroutT(6 downto 0),addstall,Clock,DoutT,Wren,DINN);
-	scroll: seg7_scroll port map(h0or,h1or,h2or,h3or,h4or,h5or,h6or,h7or,Clock,Resetn,AddroutT(6 downto 0),HEX0,HEX1,HEX2,HEX3,HEX4,HEX5,HEX6,HEX7);
+	scroll: seg7_scroll port map(h0or,Clock,Resetn,E,addstall(2 downto 0),Dout(6 downto0),HEX0,HEX1,HEX2,HEX3,HEX4,HEX5,HEX6,HEX7);
 END Behavior;
---
+++ LAB10PARTE4/port_n.vhd
@@ -0,0 +1,33 @@
+-- ES575 - Laboratório de Circuitos Lógicos
+-- Laboratório 10(Parte III) - Projeto de Máquinas de Estados Temporizadas
+-- Aluno: Carlos Vinícius Araki Oliveira - RA:160141
+-- Aluno: Cleber França Carvalho         - RA:145739
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+USE ieee.std_logic_signed.all;
+
+ENTITY port_n IS
+	PORT ( chaves : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+			memmoria: in std_logic_vector(15 downto 0);
+			sel,Clk,Clear: in STD_LOGIC;
+			saida: out std_logic_vector(15 downto 0)
+		   );
+END port_n;
+
+
+ARCHITECTURE Behavior OF parte4 IS
+
+component reg IS
+	PORT (Sel,Clk,Clear : IN STD_LOGIC;
+				data:in std_logic_vector(15 downto 0);
+			q : OUT std_logic_vector(15 downto 0));
+END component;
+signal saida1:std_logic_vector(15 downto 0);
+BEGIN
+	r1: reg port map('1',Clk,Clear,chaves,saida1);
+	saida <=  saida1 WHEN sel = '1'
+					ELSE memmoria;
+
+
+END Behavior;
